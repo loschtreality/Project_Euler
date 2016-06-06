01f9d7ce222de37f47b92b355fd2368a54a2096e
Michael Loschiavo <losch103@gmail.com>
2016-06-06 14:35:22 -0700

    Created file for Largest Product Grid and completed Special Pothagorean Triplet in Ruby and JS

--- Ruby/Special_Pothagorean_Triplet.rb
+++ Ruby/Special_Pothagorean_Triplet.rb
@@ -1,15 +1,22 @@
 # A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 #
 # a^2 + b^2 = c^2
-# For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+# For example, (3^2 + 4^2) --> (9 + 16) = 25 <-- 5^2.
 #
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
 
 
-def special_poth(number)
-  
+def trips(a,b,c)
+  (a ** 2) + (b ** 2) == c**2
 end
 
-
-p special_poth(1000)
+def special_poth(number)
+  (1..number).each do |c|
+    (1..c).each do |b|
+      (1..b).each do |a|
+        return (a * b * c) if trips(a,b,c) && (a + b + c) == number
+      end
+    end
+  end
+end
